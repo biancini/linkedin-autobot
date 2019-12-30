31376fd0a5fb721e9342e865082679fae5313116
Andrea Biancini <andrea.biancini@gmai.com>
2019-12-30 19:35:20 +0100

    Small enhancements

--- autobot/views.py
+++ autobot/views.py
@@ -3,20 +3,23 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import logging
 
-
+from social_django.models import UserSocialAuth
 from .models import Greeting
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    try:
+        token = UserSocialAuth.objects.get(user=request.user).access_token
+    except Exception:
+        token = 'unknown'
+
+    return render(request, "index.html", {'token': token})
 
 @login_required
 def login(request):
-    return render(request, "index.html")
-
+    return render(request, "index.htm")
 
 def db(request):
     greeting = Greeting()
